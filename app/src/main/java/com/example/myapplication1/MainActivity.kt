51a8c57fd897185711b4608c5be5b898b6999938
Jatin Arora <arora8331@gmail.com>
2021-02-22 16:31:29 +0530

    v1

---
+++ app/src/main/java/com/example/myapplication1/MainActivity.kt
@@ -0,0 +1,185 @@
+package com.example.myapplication1
+
+import android.app.ListActivity
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothAdapter.LeScanCallback
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothManager
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.os.Handler
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
+import android.widget.Toast
+import java.util.*
+
+
+class DeviceScanActivity : ListActivity() {
+    private var mLeDeviceListAdapter: LeDeviceListAdapter? = null
+    private var mBluetoothAdapter: BluetoothAdapter? = null
+    private var mScanning = false
+    private var mHandler: Handler? = null
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        actionBar?.setTitle(R.string.title_devices)
+        mHandler = Handler()
+        val adapter = BluetoothAdapter.getDefaultAdapter()
+        val scanner = adapter.bluetoothLeScanner
+        // Use this check to determine whether BLE is supported on the device.  Then you can
+        // selectively disable BLE-related features.
+        if (!packageManager.hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            Toast.makeText(this, R.string.ble_not_supported, Toast.LENGTH_SHORT).show()
+            finish()
+        }
+        // Initializes a Bluetooth adapter.  For API level 18 and above, get a reference to
+        // BluetoothAdapter through BluetoothManager.
+        val bluetoothManager = getSystemService(BLUETOOTH_SERVICE) as BluetoothManager
+        mBluetoothAdapter = bluetoothManager.adapter
+        // Checks if Bluetooth is supported on the device.
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(this, R.string.error_bluetooth_not_supported, Toast.LENGTH_SHORT).show()
+            finish()
+            return
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        if (!mScanning) {
+            menu.findItem(R.id.menu_stop).isVisible = false
+            menu.findItem(R.id.menu_scan).isVisible = true
+            menu.findItem(R.id.menu_refresh).actionView = null
+        } else {
+            menu.findItem(R.id.menu_stop).isVisible = true
+            menu.findItem(R.id.menu_scan).isVisible = false
+            menu.findItem(R.id.menu_refresh).setActionView(
+                    R.layout.actionbar_indeterminate_progress)
+        }
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_scan -> {
+                mLeDeviceListAdapter!!.clear()
+                scanLeDevice(true)
+            }
+            R.id.menu_stop -> scanLeDevice(false)
+        }
+        return true
+    }
+
+    override fun onResume() {
+        super.onResume()
+        // Ensures Bluetooth is enabled on the device.  If Bluetooth is not currently enabled,
+        // fire an intent to display a dialog asking the user to grant permission to enable it.
+        if (!mBluetoothAdapter!!.isEnabled) {
+            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
+        }
+        // Initializes list view adapter.
+        mLeDeviceListAdapter = LeDeviceListAdapter()
+        listAdapter = mLeDeviceListAdapter
+        scanLeDevice(true)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+        // User chose not to enable Bluetooth.
+        if (requestCode == REQUEST_ENABLE_BT && resultCode == RESULT_CANCELED) {
+            finish()
+            return
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        scanLeDevice(false)
+        mLeDeviceListAdapter!!.clear()
+    }
+
+    private fun scanLeDevice(enable: Boolean) {
+        if (enable) {
+            // Stops scanning after a pre-defined scan period.
+            mHandler!!.postDelayed({
+                mScanning = false
+                mBluetoothAdapter!!.stopLeScan(mLeScanCallback)
+                invalidateOptionsMenu()
+            }, SCAN_PERIOD)
+            mScanning = true
+            mBluetoothAdapter!!.startLeScan(mLeScanCallback)
+        } else {
+            mScanning = false
+            mBluetoothAdapter!!.stopLeScan(mLeScanCallback)
+        }
+        invalidateOptionsMenu()
+    }
+
+    // Adapter for holding devices found through scanning.
+    private inner class LeDeviceListAdapter : BaseAdapter() {
+        private val mLeDevices: ArrayList<BluetoothDevice> = ArrayList()
+        fun addDevice(device: BluetoothDevice) {
+            if (!mLeDevices.contains(device)) {
+                mLeDevices.add(device)
+            }
+        }
+
+        fun getDevice(position: Int): BluetoothDevice {
+            return mLeDevices[position]
+        }
+
+        fun clear() {
+            mLeDevices.clear()
+        }
+
+        override fun getCount(): Int {
+            return mLeDevices.size
+        }
+
+        override fun getItem(i: Int): Any {
+            return mLeDevices[i]
+        }
+
+        override fun getItemId(i: Int): Long {
+            return i.toLong()
+        }
+
+        override fun getView(i: Int, view: View, viewGroup: ViewGroup): View {
+            val view: View = view
+            val viewHolder: ViewHolder
+            // General ListView optimization code.
+            viewHolder = view.tag as ViewHolder
+            val device = mLeDevices[i]
+            val deviceName = device.name
+            if (deviceName != null && deviceName.isNotEmpty()) viewHolder.deviceName!!.text = deviceName else viewHolder.deviceName?.setText(R.string.unknown_device)
+            viewHolder.deviceAddress!!.text = device.address
+            return view
+        }
+
+    }
+
+    // Device scan callback.
+    private val mLeScanCallback = LeScanCallback { device, rssi, scanRecord ->
+        runOnUiThread {
+            mLeDeviceListAdapter!!.addDevice(device)
+            mLeDeviceListAdapter!!.notifyDataSetChanged()
+        }
+    }
+
+    internal class ViewHolder {
+        var deviceName: TextView? = null
+        var deviceAddress: TextView? = null
+    }
+
+    companion object {
+        private const val REQUEST_ENABLE_BT = 1
+
+        // Stops scanning after 10 seconds.
+        private const val SCAN_PERIOD: Long = 10000
+    }
+}
